9a28e1a11b45c812d026d9d6df1b7b1625b89cbe
Zalexanninev15 <blue.shark@disroot.org>
2020-03-17 20:28:37 +0300

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo ***Adobe SWF \& FLW Player Installer by Zalexanninev15***
+echo ***Adobe SWF \& FLV Player Installer by Zalexanninev15***
 wget https://github.com/Zalexanninev15/Adobe-SWF_FLW-Player-Installer/raw/master/swf-player.zip
 unzip swf-player.zip
 rm swf-player.zip
