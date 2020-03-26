4c7b2ed1f991d7edd4011dc9d324e72505aedd73
Zalexanninev15 <blue.shark@disroot.org>
2020-03-26 11:09:55 +0300

    Delete installer.sh

--- installer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo ***Adobe SWF \& FLV Player Installer by Zalexanninev15***
-wget https://github.com/Zalexanninev15/Adobe-SWF_FLW-Player-Installer/raw/master/swf-player.zip
-unzip swf-player.zip
-rm swf-player.zip
-sudo cp swf-player /usr/bin/swf-player
-rm swf-player
